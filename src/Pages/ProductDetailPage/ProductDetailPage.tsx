c62c220f6dddc90d563ec5340c77266a92834778
Simon Gonzalez <simongonzalezechavarria@gmail.com>
2022-10-27 09:48:03 -0500

    feat: product detail page

---
+++ src/Pages/ProductDetailPage/ProductDetailPage.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import MercadoLibreClient from "../../Client/MercadoLibre.client";
+import Header from "../../Components/Shared/Header/Header";
+import { ProductDescription } from "../../Typings/Details/Description";
+import { DetailProducts } from "../../Typings/Details/Details";
+import "./ProductDetailPage.scss";
+
+const ProductDetailPage = () => {
+  const [detailsProduct, setDetailsProduct] = useState<DetailProducts>();
+  const [Description, setDescription] = useState<ProductDescription>();
+
+  let { id } = useParams();
+
+  useEffect(() => {
+    if (id) {
+      MercadoLibreClient.getProductDetail(id).then((result) => {
+        setDetailsProduct(result);
+      });
+      MercadoLibreClient.getProductDescription(id).then((result) => {
+        setDescription(result);
+      });
+    }
+  }, [id]);
+
+  return (
+    <>
+      <Header />
+      <div className="product-detail-page">
+        <div className="product-detail-page-detail">
+          <div className="product-detail-page-detail-image">
+            <img src={detailsProduct?.thumbnail} alt={detailsProduct?.title} />
+          </div>
+          <div className="product-detail-page-detail-info">
+            <p className="product-detail-page-detail-info-condition">
+              {detailsProduct?.condition === "new" ? "Nuevo" : "Usado"} -{" "}
+              {detailsProduct?.sold_quantity} vendidos
+            </p>
+            <p className="product-detail-page-detail-info-title">
+              {detailsProduct?.title}
+            </p>
+            <p className="product-detail-page-detail-info-price">
+              $ {detailsProduct?.price}
+            </p>
+            <button className="product-detail-page-detail-info-button">
+              Comprar
+            </button>
+          </div>
+        </div>
+        <div className="product-detail-page-description">
+          <p className="product-detail-page-description-title">
+            Descripción del producto
+          </p>
+          <p className="product-detail-page-description-text">
+            {Description?.plain_text}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductDetailPage;
--- src/Pages/ProductListPage/Card/Card.tsx
+++ src/Pages/ProductListPage/Card/Card.tsx
@@ -7,14 +7,16 @@ const Card = (product: Product) => {
     <div className="card">
       <Link to={`/items/${product.id}`}>
         <img src={product.thumbnail} alt={product.title} />
-        <div className="card__info">
-          <div className="card__info__price">
-            <p>$ {product.prices.prices[0].amount}</p>
-            {product.shipping && <img src={FreeShipping} alt="free_shipping" />}
-          </div>
-          <p className="card__info__title">{product.title}</p>
-        </div>
       </Link>
+      <div className="card__info">
+        <div className="card__info__price">
+          <p>$ {product.prices.prices[0].amount}</p>
+          {product.shipping && <img src={FreeShipping} alt="free_shipping" />}
+        </div>
+        <Link to={`/items/${product.id}`}>
+          <p className="card__info__title">{product.title}</p>
+        </Link>
+      </div>
       <div className="card__location">
         <p>{product.address.state_name}</p>
       </div>
