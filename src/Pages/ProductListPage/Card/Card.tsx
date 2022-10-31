200562391c250166aee46e33003dc4041c03636e
Simon Gonzalez <simongonzalezechavarria@gmail.com>
2022-10-30 23:12:42 -0500

    fix: data card valid

--- src/Pages/ProductListPage/Card/Card.tsx
+++ src/Pages/ProductListPage/Card/Card.tsx
@@ -6,20 +6,20 @@ import Format from "../../../Utils/Format";
 const Card = (product: Product) => {
   return (
     <div className="card">
-      <Link to={`/items/${product.id}`}>
-        <img src={product.thumbnail} alt={product.title} />
+      <Link to={`/items/${product?.id}`}>
+        <img src={product?.thumbnail} alt={product?.title} />
       </Link>
       <div className="card__info">
         <div className="card__info__price">
-          <p>{Format.Price(product.prices.prices[0].amount, product.currency_id)}</p>
-          {product.shipping && <img src={FreeShipping} alt="free_shipping" />}
+          <p>{Format.Price(product?.prices?.prices?.[0]?.amount, product?.currency_id)}</p>
+          {product?.shipping && <img src={FreeShipping} alt="free_shipping" />}
         </div>
         <Link to={`/items/${product.id}`}>
-          <p className="card__info__title">{product.title}</p>
+          <p className="card__info__title">{product?.title}</p>
         </Link>
       </div>
       <div className="card__location">
-        <p>{product.address.state_name}</p>
+        <p>{product?.address?.state_name}</p>
       </div>
     </div>
   );
